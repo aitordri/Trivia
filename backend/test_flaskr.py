b87fc2f6a5c895a39f05d9f9261ef5417fb993be
Aitor <soy@aitor.me>
2020-03-22 16:48:51 +0100

    first test: get questions

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -14,7 +14,7 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_path = "postgres://postgres:root@localhost:5432/trivia"
+        self.database_path = "postgresql://postgres:root@localhost:5432/trivia"
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -33,6 +33,16 @@ class TriviaTestCase(unittest.TestCase):
     Write at least one test for each test for successful operation and for expected errors.
     """
 
+    def test_get_questions(self):
+
+        response = self.client().get('/questions')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(len(data['questions']))
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
